17f6af7d4dfdff5918dc902a952c893bf7104101
jack-marken <jackmarken9@gmail.com>
2025-05-31 15:21:49 +1000

    First DB connection

--- ArrowMate/public/index.php
+++ ArrowMate/public/index.php
@@ -1,9 +1,18 @@
 <?php
+  $database = 's105584279_db';
+  $username = 's105584279';
+  $password = '210605';
+  $dbh = new PDO("mysql:host=feenix-mariadb.swin.edu.au;dbname=$database", $username, $password);
+  foreach($dbh->query('SELECT * from Archer') as $row)
+  {
+     print_r($row);
+  }
 ?>
 <html>
 <head>
   <title>Index</title>
 </head>
 <body>
+  <h1>Testing</h1>
 </body>
 </html>
