033cd18f682e234ef91d8b6a26b3fb29b644e6f1
jack-marken <jackmarken9@gmail.com>
2025-06-01 00:45:33 +1000

    Update nav code layout

--- ArrowMate/public/components/bottom-nav.php
+++ ArrowMate/public/components/bottom-nav.php
@@ -11,10 +11,29 @@
   <button class="nav-btn" data-goto="leaderboard" ><i class="fa-solid fa-trophy"></i></button>
   <button class="nav-btn" data-goto="profile"     ><i class="fa-solid fa-user"></i></button>
 -->
+<nav class="bottom-nav">
+  <!-- 1 ▸ HOME -->
+  <a class="nav-btn" data-goto="home">
+    <i class="fa-solid fa-house"></i>
+  </a>
 
-  <a class="nav-btn" href="your-scores"><i class="fa-solid fa-house"></i></a>
-  <a class="nav-btn" href="recorder"    ><i class="fa-solid fa-clipboard-list"></i></a>
-  <a class="nav-btn nav-btn--target center" href="actions"><i class="fa-solid fa-bullseye"></i></a>
-  <a class="nav-btn" href="leaderboard" ><i class="fa-solid fa-trophy"></i></a>
-  <a class="nav-btn" href="profile"     ><i class="fa-solid fa-user"></i></a>
+  <!-- 2 ▸ ROUND DETAILS / YOUR SCORES -->
+  <a class="nav-btn" data-goto="your-scores">
+    <i class="fa-solid fa-clipboard-list"></i>
+  </a>
+
+  <!-- 3 ▸ RECORD / JOIN A RANGE (centre target button) -->
+  <a class="nav-btn nav-btn--target center" data-goto="actions">
+    <i class="fa-solid fa-bullseye"></i>
+  </a>
+
+  <!-- 4 ▸ LEADERBOARD -->
+  <a class="nav-btn" data-goto="leaderboard">
+    <i class="fa-solid fa-trophy"></i>
+  </a>
+
+  <!-- 5 ▸ PROFILE -->
+  <a class="nav-btn" data-goto="profile">
+    <i class="fa-solid fa-user"></i>
+  </a>
 </nav>
