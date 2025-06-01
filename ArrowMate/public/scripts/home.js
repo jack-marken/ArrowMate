0f32fce7240d0ce5f128c516980faa21bde94ef6
jack-marken <jackmarken9@gmail.com>
2025-06-01 14:27:12 +1000

    Rename script.js -> home.js

--- ArrowMate/public/scripts/home.js
+++ ArrowMate/public/scripts/home.js
@@ -5,16 +5,22 @@ DEFAULT_HOME_PAGE = 'home';
 // in the URL toggles their '.active' class ('display: none;' -> 'display: flex;')
 
 function SetPage(page) {
-  window.history.pushState({ page: 1 }, "ArrowScores", "?page=" + page);
+  // lastPartOfUrl = $(location).attr('href').split('/').at(-1);
+  // alert(lastPartOfUrl);
+  // .substring(thePath.lastIndexOf('/') + 1)
+  window.history.pushState({ page: page }, "ArrowScores", "?page=" + page);
 }
 
 function ShowPage() {
+  // Parse the '?page=' query in the URL to find the active page
+  // If it does not exist, set the default page
   var urlParams = new URLSearchParams(window.location.search);
   activePage = urlParams.get('page');
   if (!activePage) {
     SetPage(DEFAULT_HOME_PAGE);
     activePage = DEFAULT_HOME_PAGE;
   }
+  // Display the active page
   $('.screen').each(function () {
     if ($(this).attr('id') == activePage) {
       $(this).addClass('active');
@@ -22,8 +28,20 @@ function ShowPage() {
       $(this).removeClass('active');
     }
   });
+  // Set the nav button to an active colour
+  $('.nav-btn').each(function () {
+    if ($(this).data('goto') == activePage) {
+      $(this).addClass('nav-active');
+    } else {
+      $(this).removeClass('nav-active');
+    }
+  });
 }
 ShowPage();
+
+$(window).on('popstate', function(e) {
+  ShowPage();
+});
 
 $('.nav-btn').each(function () {
   var $navBtn = $(this);
