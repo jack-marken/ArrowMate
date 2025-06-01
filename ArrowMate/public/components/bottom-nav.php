0a197724e28d43b8ff6f962df3b49e267663f6fa
jack-marken <jackmarken9@gmail.com>
2025-06-01 12:16:17 +1000

    Remove unused comments

--- ArrowMate/public/components/bottom-nav.php
+++ ArrowMate/public/components/bottom-nav.php
@@ -2,16 +2,6 @@
   BOTTOM NAV
 ----------------------------------------------------------------->
 <nav class="bottom-nav">
-  <!-- <button class="nav-btn" data-goto="home"        ><i class="fa-solid fa-house"></i></button> -->
-
-<!--
-  <button class="nav-btn" data-goto="your-scores"        ><i class="fa-solid fa-house"></i></button>
-  <button class="nav-btn" data-goto="recorder"    ><i class="fa-solid fa-clipboard-list"></i></button>
-  <button class="nav-btn nav-btn--target center" data-goto="actions"><i class="fa-solid fa-bullseye"></i></button>
-  <button class="nav-btn" data-goto="leaderboard" ><i class="fa-solid fa-trophy"></i></button>
-  <button class="nav-btn" data-goto="profile"     ><i class="fa-solid fa-user"></i></button>
--->
-<nav class="bottom-nav">
   <!-- 1 ▸ HOME -->
   <button class="nav-btn" data-goto="home">
     <i class="fa-solid fa-house"></i>
@@ -23,7 +13,7 @@
   </button>
 
   <!-- 3 ▸ RECORD / JOIN A RANGE (centre target button) -->
-  <button class="nav-btn nav-btn--target center" data-goto="action-picker">
+  <button class="nav-btn nav-btn--target center" data-goto="join-range">
     <i class="fa-solid fa-bullseye"></i>
   </button>
 
